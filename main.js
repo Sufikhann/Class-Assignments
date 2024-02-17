b5458d576a676426bc260611dc3fff6d9d3fed21
Sufikhann <160335860+Sufikhann@users.noreply.github.com>
2024-02-17 20:21:34 +0500

    this is my first files

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log('Hello World');
+console.log('hello sufi');
+var a = 15;
+var b = 20;
+console.log(a + b);
---
+++ main.ts
@@ -0,0 +1,11 @@
+ console.log('Hello World');
+ console.log('hello sufi');
+ 
+ let a =15
+ let b=20
+ console.log(a+b);
+ 
+
+
+ 
+ 
